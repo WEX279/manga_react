072a6f0e2d5dd8aa3d60081555c98fb781b4d51c
WEX_279 <legazyuri279@gmail.com>
2026-06-17 17:54:41 +0200

    feat: add basic react structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Greetings } from './Components/Greetings'
+
+function App() {
+
+  return (
+   Greetings
+  )
+}
+
+export default App
---
+++ src/Components/Greetings.jsx
@@ -0,0 +1,5 @@
+function Greetings(){
+    return <h1>pumba</h1>
+}
+
+export { Greetings }
