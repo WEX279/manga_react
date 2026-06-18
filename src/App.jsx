48dae3c620943b33be05ee5258b2de3ccd3538e5
WEX_279 <legazyuri279@gmail.com>
2026-06-18 16:16:46 +0200

    feat: add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
-import { Greetings } from './Components/Greetings'
+import { Routes, Route, Link } from 'react-router-dom'
+import { Home } from './Pages/Home'; 
+import { About } from './Pages/About';
+import { Characters } from './Pages/Characters';
 
 function App() {
 
   return (
-   Greetings
+    <div>
+      <nav>
+        <Link to="/Home">Inicio</Link> | <Link to="/About">About us</Link> | <Link to="/Characters">Personajes</Link>
+      </nav>
+
+      <Routes>
+        <Route path='/Home' element={<Home/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/Characters' element={<Characters/>}/>
+      </Routes>
+    </div>
   )
 }
 
-export default App
+export {
+  App
+}
--- src/Components/Greetings.jsx
+++
@@ -1,5 +0,0 @@
-function Greetings(){
-    return <h1>pumba</h1>
-}
-
-export { Greetings }
