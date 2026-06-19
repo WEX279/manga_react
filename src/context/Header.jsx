29739e65eafeb9d0fcb0510387ba46277ac4a51e
WEX_279 <legazyuri279@gmail.com>
2026-06-19 17:15:15 +0200

    fix base page css

--- src/context/Header.jsx
+++ src/context/Header.jsx
@@ -2,7 +2,12 @@ import { useContext } from "react";
 import { SwitchTheme } from "../Components/ThemeSwitcher";
 function Header() {
     const theme = useContext(SwitchTheme)
-    return<header className={theme}>Mi app - modo {theme}</header>
+    return(
+    <div>
+        <header className={theme}>Mi app - modo {theme}</header>
+        <SwitchTheme/>
+    </div>
+    )
 }
 
 export{
