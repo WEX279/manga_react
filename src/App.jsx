88c01e7d4cd57e9700cd9cf222050c3ec866878f
WEX_279 <legazyuri279@gmail.com>
2026-06-28 01:09:39 +0200

    feat add profile page (placeholder)

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import { Navbar } from './Components/Organisms/Navbar';
 import { Home } from './Pages/Home';
 import { Catalog } from './Pages/Catalog';
 import { MangaCard } from './Pages/MangaCard';
-import { Signup } from './Pages/SignUp';
 import { ThemeContext } from './context/ThemeContext';
 import { RegisterForm } from './Pages/RegisterForm';
+import { SignUpForm } from './Pages/SignUpForm';
 import { LoginForm } from './Pages/LoginForm';
 import { PrivateRoute } from './Components/PrivateRoute';
 import { About } from './Pages/About';
+import { Profile } from './Pages/Profile';
 
 function App() {
   const [theme, setTheme] = useState('☀️')
@@ -22,13 +23,15 @@ function App() {
             <Route path='/' element={<Navbar/>}>
               <Route path='/home' element={<Home/>}/>
               <Route path='/manga' element={<Catalog/>}/>
-              <Route path='/manga/:_id' element={<MangaCard/>}/>
               <Route element={<PrivateRoute/>}>
+                <Route path='/manga/:_id' element={<MangaCard/>}/>
                 <Route path='/about' element={<About/>}/>
+                <Route path='/account' element={<Profile/>}/>
+
               </Route>
             </Route>  
 
-          <Route path='/signup' element={<Signup/>}/>
+          <Route path='/signup' element={<SignUpForm/>}/>
           <Route path='/login' element={<LoginForm/>} />
           <Route path='/retest' element={<RegisterForm/>} />
 
---
+++ src/Pages/Profile.jsx
@@ -0,0 +1,11 @@
+function Profile() {
+    return(
+        <div>
+            <p>a</p>
+        </div>
+    )
+}
+
+export{
+    Profile
+}
