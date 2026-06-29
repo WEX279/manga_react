6732417f6ea5a69752ac29aba62e7682ed4e8624
WEX_279 <legazyuri279@gmail.com>
2026-06-29 13:53:40 +0200

    refactor token save for darkmode

--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,10 @@ import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
-		<AuthProvider>
-			<BrowserRouter>
-					<App />
-			</BrowserRouter>
-		</AuthProvider>
+		<BrowserRouter>
+				<AuthProvider>
+						<App />
+				</AuthProvider>
+		</BrowserRouter>
     </StrictMode>
 )
