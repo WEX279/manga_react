b3010268e4aa7b5ffa9c5a28e7f4f6d15361e38a
WEX_279 <legazyuri279@gmail.com>
2026-06-30 04:08:20 +0200

    refactor home page

--- src/Components/Atoms/Banner.jsx
+++ src/Components/Atoms/Banner.jsx
@@ -4,27 +4,25 @@ import { TextPlaceholder } from "./TextPlaceholder"
 
 function Banner(){
     return(
-        <div className=" bg-Dark light:bg-Light min-h-[calc(100vh-69px)] ">
+        <div className=" bg-Dark light:bg-Light">
             <div className="flex justify-center flex-col ">
-                    <section className="flex flex-col relative self-center max-h-[50vh]">
-                        <img src={BookShelf} className="rounded-4xl w-[90vw] light:border-Dark opacity-70"/>
+                    <section className="flex flex-col ">
+                        <img src={BookShelf} className="rounded-4xl w-[90vw] light:border-Dark opacity-70 self-center "/>
 
-                        <div className=" flex absolute inset-0 items-center p-[2em] text-fourth light:text-fourthAlt">
-                            <h1 className="font-bold text-[4em] ">Descubre tu nuevo manga favorito, aquí.</h1>
+                        <div className=" flex  p-[2em] text-fourth light:text-fourthAl justify-center">
+                            <h1 className="flex font-bold text-[4em]">Descubre tu nuevo manga favorito, aquí.</h1>
                         </div>
                     </section>
-                    <div>
-                        <section className="text-Light light:text-Dark p-[3em] flex ">
-                            <div className="flex flex-col max-w-[50vw]">
-                                <h2 className="text-fourth font-bold text-[2em] light:text-fourthAlt">Chatgpt es subnormal</h2>
-                                <TextPlaceholder/>
-                            </div>
+                    <section className="text-Light light:text-Dark p-[3em] flex ">
+                        <div className="flex flex-col max-w-[50vw]">
+                            <h2 className="text-fourth font-bold text-[2em] light:text-fourthAlt">Chatgpt es subnormal</h2>
+                            <TextPlaceholder/>
+                        </div>
 
-                            <div className="flex justify-center self-start p-[3em]">
-                                <img src={Kojima}/>
-                            </div>
-                        </section>
-                    </div>
+                        <div className="flex justify-center self-start p-[3em]">
+                            <img src={Kojima}/>
+                        </div>
+                    </section>
             </div>
         </div>
     )
--- src/Components/Organisms/Footer.jsx
+++ src/Components/Organisms/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return(
-        <div className="border-t-Light light:border-t-Dark border-t bg-Dark light:bg-Light text-Light fixed bottom-0 min-w-screen">
+        <div className="border-t-Light light:border-t-Dark border-t bg-Dark light:bg-Light text-Light">
             <div className="p-[1em] text-Light light:text-Dark ">
                 <p className="font-bold text-[2em]">Credits</p>
                 <p className="font-mono">WEX_279</p>
