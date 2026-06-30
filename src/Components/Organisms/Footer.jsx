fac078053f6a09b69ec4da3d24f020472c41a35a
WEX_279 <legazyuri279@gmail.com>
2026-06-30 03:21:10 +0200

    refactor footer

--- src/Components/Organisms/Footer.jsx
+++ src/Components/Organisms/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return(
-        <div className="border-t-Light light:border-t-Dark border-t bg-Dark light:bg-Light text-Light ">
-            <div className="p-[1em] text-Light light:text-Dark">
+        <div className="border-t-Light light:border-t-Dark border-t bg-Dark light:bg-Light text-Light fixed bottom-0 min-w-screen">
+            <div className="p-[1em] text-Light light:text-Dark ">
                 <p className="font-bold text-[2em]">Credits</p>
                 <p className="font-mono">WEX_279</p>
 
