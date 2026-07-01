c53ca4e82376ac32994a26719ea8a7299a1b8909
WEX_279 <legazyuri279@gmail.com>
2026-07-01 20:43:55 +0200

    refactor abt

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,14 @@
-import { TextPlaceholder } from "../Components/Atoms/TextPlaceholder"
-
 function About(){
     return(
         <div className="bg-Dark light:bg-Light text-Light light:text-Dark h-screen">
-            <h1 className="font-bold text-[3em] p-7 ">About Us!</h1>
-            <TextPlaceholder/>
+            <section className="p-7">
+                <h1 className="font-bold text-[3em] ">About Us!</h1>
+                <p>MangaSync  es un proyecto académico llevado a cabo con el fin de practicar el desarrollo de Frontend con react así cómo lidiar con peticiones de usuarios conectando con nuestro propio backend</p>
+                <br/>
+                <p>Esta página web está diseñada para gestionar mangas y usuarios, permitiendo que estos puedan visitar los mangas que deseen siempre y cuando tengan una cuenta. La web permite crear cuentas de usuario, iniciar sesión de forma segura, y revisar tarjetas con información de los mangas.</p>
+                <br/>
+                <p>El proyecto está enfocado en el desarrollo frontend utilizando React, Vite y Tailwind</p>
+            </section>
         </div>
     )
 }
